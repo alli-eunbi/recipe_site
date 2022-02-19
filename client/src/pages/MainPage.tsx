b39846771a04ae157838d67324d0ea37dd9a398e
d_flex <hiyoo95@gmail.com>
2022-02-19 19:12:07 +0900

    feat/hwanik - update: 메인페이지 Swiper 액션 적용

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -2,37 +2,25 @@ import React, { MouseEventHandler, useState } from 'react';
 import styled from 'styled-components';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import { Mousewheel, Pagination } from 'swiper';
-import { useQuery } from 'react-query';
-import { testRequest } from '../api/test';
 import { Title } from '../components/text/Title';
 import { mainPageText } from '../assets/data/mainPageText';
 
 const MainPage: React.FC = () => {
-  /* axios.get 코드는 'client/src/api/test.ts'  에 있습니다. */
-
-  const { data, isLoading, refetch } = useQuery('test', testRequest, {
-    enabled: false,
-  });
-
-  const handleFetch = () => {
-    refetch();
-  };
-
-  console.log(data?.data);
-
   return (
     <Swiper
       modules={[Mousewheel, Pagination]}
       slidesPerView={1}
       mousewheel={true}
-      pagination={{ clickable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
     >
       {mainPageText.map((text) => (
         <SwiperSlide key={text.title}>
           <MainDisplay>
+            <div></div>
             <div>
               <Title>{text.title}</Title>
               <h2>{text.subTitle}</h2>
