8a0faaff39ebb71fbdd9fecbd2864e5222b19c68
d_flex <hiyoo95@gmail.com>
2022-02-23 23:17:56 +0900

    feat/hwanik - update : 소셜 로그인 기능 구현 완료 =>  로그인 시 authenticated 설정 및 쿠키에 jwt 저장

--- client/src/pages/oath/KakaoRedirectPage.tsx
+++ client/src/pages/oath/KakaoRedirectPage.tsx
@@ -5,17 +5,26 @@ import { sendKakaoAuthCode } from '../../api/user';
 import { Navigate } from 'react-router-dom';
 import { PageLayout } from '../../components/layout/PageLayout';
 import { useCookies } from 'react-cookie';
+import { authAtom } from '../../store/store';
+import { useSetRecoilState } from 'recoil';
 
 const KakaoRedirectPage: React.FC = () => {
   const [cookie, setCookie] = useCookies(['jwt']);
+  const setAuthenticated = useSetRecoilState(authAtom);
+
   const authCode = new URL(window.location.href).searchParams.get('code');
 
-  const { data: token, isFetched } = useQuery('send-authCode', () =>
-    sendKakaoAuthCode(authCode)
+  const { data: token, isFetched } = useQuery(
+    'send-authCode',
+    () => sendKakaoAuthCode(authCode),
+    {
+      cacheTime: 0,
+    }
   );
 
   if (isFetched) {
     setCookie('jwt', token?.data.jwt);
+    setAuthenticated(true);
     return <Navigate to='/' />;
   }
 
