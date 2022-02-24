56c342838ea18d393ab334b677e37e4dc270181c
Jin-Mook <jink0608@naver.com>
2022-02-24 23:06:53 +0900

    이미지 저장 및 전송 테스트

--- server/routes/recipe_board.py
+++ server/routes/recipe_board.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, g
+from flask import Blueprint, jsonify, request, g, send_file
 from flask_restx import Namespace, Resource, fields
 from models import db, Users
 import os
@@ -14,12 +14,26 @@ class Recipe_register(Resource):
   def post(self):
     # 데이터 전달
     request_form = request.form
+    print('request_form: ', request_form)
 
     files = request.files.get('file')
     texts = request_form.get('data')
 
-    print('files: ', files)
+    full_filename = files.filename
+    extension = full_filename.split('.')[-1]
+    filename = '20220225'
+    save_file_name = f"static/{filename}.{extension}"
+    print('file: ', save_file_name)
     print('texts: ', texts)
-
-
+    # static폴더에 파일 저장
+    files.save(save_file_name)
+    
     return 'ok'
+
+
+@recipe_board_page_api.route('/test')
+class Test(Resource):
+  def get(self):
+    data = {"success": True, "url": "localhost:5000/static/20220225.png"}
+    
+    return jsonify(data)
