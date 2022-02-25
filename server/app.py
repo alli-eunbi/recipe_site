f30dde351c3f2aad95759eff7d3e0298fd014049
Jin-Mook <jink0608@naver.com>
2022-02-26 02:09:28 +0900

    app.py routes.search import문 수정

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from routes.login import login_page, login_page_api
 from routes.socialLogin import social_login_page, social_login_page_api
 from routes.recipeDetail import recipe_detail, recipe_detail_api
 from routes.recipe_board import recipe_board_page, recipe_board_page_api
-from routes.search import search, recipes_search_api, recipe_board_page, recipe_board_page_api
+from routes.search import search, recipes_search_api
 from dotenv import load_dotenv
 import os
 import jwt
