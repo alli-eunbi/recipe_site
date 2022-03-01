2d09a5b642e11fad5d5b25adcb81570f61d700c5
d_flex <hiyoo95@gmail.com>
2022-03-01 21:38:15 +0900

    소셜 로그인 시, 네비게이션바 로고 이미지 깨짐 수정

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -3,14 +3,12 @@ import styled from 'styled-components';
 import Header from './Header';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import HeaderLogo from './HeaderLogo';
 
 const Navigation: React.FC = () => {
   return (
     <Header>
-      <Logo to='/'>
-        <img src='/images/vegan_logo.svg' alt='logo' />
-        <span>한컷한상</span>
-      </Logo>
+      <HeaderLogo />
       <nav>
         <NavLinks />
       </nav>
@@ -19,20 +17,3 @@ const Navigation: React.FC = () => {
 };
 
 export default Navigation;
-
-const Logo = styled(Link)`
-  margin-left: 1rem;
-  text-decoration: none;
-  color: white;
-  display: flex;
-  align-items: center;
-
-  & > img {
-    height: 3rem;
-  }
-
-  & > span {
-    margin-left: 0.5rem;
-    font-size: 1.5rem;
-  }
-`;
