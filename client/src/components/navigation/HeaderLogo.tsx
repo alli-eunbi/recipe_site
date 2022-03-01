b0c2452482fec5e6c0533d8faaa2639cdac4b75d
d_flex <hiyoo95@gmail.com>
2022-03-01 21:38:15 +0900

    소셜 로그인 시, 네비게이션바 로고 이미지 깨짐 수정

---
+++ client/src/components/navigation/HeaderLogo.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const HeaderLogo = () => {
+  return (
+    <Logo to='/'>
+      <img src='/images/vegan_logo.svg' alt='logo' />
+      <span>한컷한상</span>
+    </Logo>
+  );
+};
+
+export default HeaderLogo;
+
+const Logo = styled(Link)`
+  margin-left: 1rem;
+  text-decoration: none;
+  color: white;
+  display: flex;
+  align-items: center;
+
+  & > img {
+    height: 3rem;
+  }
+
+  & > span {
+    margin-left: 0.5rem;
+    font-size: 1.5rem;
+  }
+`;
