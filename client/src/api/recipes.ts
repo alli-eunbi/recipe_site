8df9d651568af7cd4b235038c34e8d24a7114925
d_flex <hiyoo95@gmail.com>
2022-03-04 18:42:15 +0900

    board 작성시, cookie 실어 보내기

--- client/src/api/recipes.ts
+++ client/src/api/recipes.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 export const sendIngredientPhoto = (formData: FormData) => {
   return axios.post(`${process.env.REACT_APP_BASE_URL}/`, formData);
@@ -9,7 +10,14 @@ export const fetchDetailInfo = (params: string | undefined) => {
 };
 
 export const registerRecipe = (formData: FormData) => {
-  return axios.post(
+  const cookie = new Cookies().get('access_token');
+
+  const header = axios.create({
+    headers: {
+      Authorization: cookie,
+    },
+  });
+  return header.post(
     `${process.env.REACT_APP_BASE_URL}/recipe-board/register`,
     formData
   );
