b9801c1a02e3f126d7e5635738139742dae62547
d_flex <hiyoo95@gmail.com>
2022-03-08 15:25:29 +0900

    구매 사이트 이동 링크 삽입

--- client/src/components/recipes/details/ShopIngredients.tsx
+++ client/src/components/recipes/details/ShopIngredients.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
-const ShopIngredients: React.FC = () => {
-  return <div></div>;
+type Props = {
+  ingredients: string[];
+};
+
+const ShopIngredients: React.FC<Props> = ({ ingredients }) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', margin: '2rem' }}>
+      <h3>재료를 사야하나요?</h3>
+      {ingredients.map((ingredient: string) => (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {ingredient}:
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <a
+              href={`https://www.oasis.co.kr/product/search?keyword=${ingredient}`}
+            >
+              오아시스 마켓
+            </a>
+            <a
+              href={`https://www.ssg.com/search.ssg?target=all&query=${ingredient}`}
+            >
+              SSG.COM
+            </a>
+            <a
+              href={`https://www.coupang.com/np/search?component=&q=${ingredient}`}
+            >
+              쿠팡
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ShopIngredients;
